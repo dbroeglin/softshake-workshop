1f45acdce38858d5cc730f4597e6e9c5824e4752
Dominique Broeglin <dominique.broeglin@gmail.com>
2013-10-24 16:49:56 +0200

    Added Projects and association with Tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131024141855) do
+ActiveRecord::Schema.define(version: 20131024144844) do
+
+  create_table "projects", force: true do |t|
+    t.string   "title"
+    t.boolean  "completed"
+    t.date     "due_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "tasks", force: true do |t|
     t.string   "title"
     t.boolean  "completed"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "project_id"
   end
 
 end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "add project to task" do
+    task = Task.new(title: 'do it')
+    task.project = projects(:one)
+    task.save
+
+    assert Task.find_by_project_id(projects(:one).id) == task
+  end
 end
